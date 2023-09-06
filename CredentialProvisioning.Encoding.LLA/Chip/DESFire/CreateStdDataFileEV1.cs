b53029b86f66c67e5257c1ebb8694a397ed941e3
Maxhy <maxime.chamley@leosac.com>
2023-09-06 22:12:02 +0200

    Add CreateStdDataFileEV1 action

---
+++ CredentialProvisioning.Encoding.LLA/Chip/DESFire/CreateStdDataFileEV1.cs
@@ -0,0 +1,20 @@
+﻿using LibLogicalAccess.Card;
+
+namespace Leosac.CredentialProvisioning.Encoding.LLA.Chip.DESFire
+{
+    public class CreateStdDataFileEV1 : DESFireEV1Action<Leosac.CredentialProvisioning.Encoding.Chip.DESFire.CreateStdDataFileEV1>
+    {
+        public CreateStdDataFileEV1(Leosac.CredentialProvisioning.Encoding.Chip.DESFire.CreateStdDataFileEV1 properties) : base(properties)
+        {
+
+        }
+
+        public override void RunDESFireEV1(DESFireEV1Commands cmd, EncodingContext encodingCtx, LLACardContext cardCtx)
+        {
+            if (Properties.IsoFID != null)
+                cmd.createStdDataFile(Properties.FileNo, (EncryptionMode)Properties.EncryptionMode, Properties.AccessRights.ConvertForLLA(), Properties.FileSize, Properties.IsoFID.Value);
+            else
+                cmd.createStdDataFile(Properties.FileNo, (EncryptionMode)Properties.EncryptionMode, Properties.AccessRights.ConvertForLLA(), Properties.FileSize);
+        }
+    }
+}
---
+++ CredentialProvisioning.Encoding/Chip/DESFire/CreateStdDataFileEV1.cs
@@ -0,0 +1,19 @@
+﻿
+namespace Leosac.CredentialProvisioning.Encoding.Chip.DESFire
+{
+    /// <summary>
+    /// Create a new standard data file EV1.
+    /// </summary>
+    public class CreateStdDataFileEV1 : CreateStdDataFile
+    {
+        /// <summary>
+        /// See <see cref="EncodingActionProperties.Name" />.
+        /// </summary>
+        public override string Name => "Create Standard Data File EV1";
+
+        /// <summary>
+        /// Optional ISO FID.
+        /// </summary>
+        public ushort? IsoFID { get; set; }
+    }
+}
