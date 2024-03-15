567f232fda65b3eda4ec766394c007cc065b09c8
Maxhy <maxime@leosac.com>
2024-03-15 12:28:21 +0100

    Use dedicated leosac card icon

--- CredentialProvisioning.Encoding/Chip/UltralightC/UltralightCActionProperties.cs
+++ CredentialProvisioning.Encoding/Chip/UltralightC/UltralightCActionProperties.cs
@@ -1,7 +1,7 @@
 ﻿namespace Leosac.CredentialProvisioning.Encoding.Chip.UltralightC
 {
     /// <summary>
-    /// Base class for Ultralight C Encoding Action Properties
+    /// Base class for Mifare Ultralight C Encoding Action Properties
     /// </summary>
     public abstract class UltralightCActionProperties : EncodingActionProperties
     {
